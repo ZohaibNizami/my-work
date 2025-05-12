ba9cd722c03b142a1ab4fe4bccfe3a050e28d64e
Muhammad Zohaib Nizami <96146561+ZohaibNizami@users.noreply.github.com>
2025-05-13 01:23:11 +0500

    updated

---
+++ backend/databases/new_data.py
@@ -0,0 +1,20 @@
+
+# (env) mona@Monas-MacBook-Pro mywork % git pull
+# There is no tracking information for the current branch.
+# Please specify which branch you want to merge with.
+# See git-pull(1) for details.
+
+#     git pull <remote> <branch>
+
+# If you wish to set tracking information for this branch you can do so with:
+
+#     git branch --set-upstream-to=origin/<branch> codwith-z
+
+# (env) mona@Monas-MacBook-Pro mywork % git push
+# fatal: The current branch codwith-z has no upstream branch.
+# To push the current branch and set the remote as upstream, use
+
+#     git push --set-upstream origin codwith-z
+
+# To have this happen automatically for branches without a tracking
+# upstream, see 'push.autoSetupRemote' in 'git help config'.
